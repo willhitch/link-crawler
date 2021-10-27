0aac3b05059e34322a309a035243803218c035ba
willhitch <hitch.will@gmail.com>
2021-10-27 15:32:26 -0700

    sort issues still persist

--- crawler.js
+++ crawler.js
@@ -1,210 +1,208 @@
-/* =====================================================================================
- Blog and Guide Product Recommendations
- ======================================================================================*/
+if (document.getElementById("p1")) {
+	// Variables
+	var links = document.getElementsByTagName("a");
+	var linkArr = [];
+	var productList = document.getElementById("product-list");
+	var productDiv = document.getElementById("p1");
 
-// Variables
-var links = document.getElementsByTagName("a");
-var linkArr = [];
-var productList = document.getElementById("product-list");
-var productDiv = document.getElementById("p1");
+	// Keep Track Of Product links
+	var rvDigestItLiquid = 0;
+	var rvDigestItPowder = 0;
+	var rvDigestItDropIn = 0;
+	var rvToiletCleaner = 0;
+	var tankCleaner = 0;
+	var sensorCleaner = 0;
+	var tankOdor = 0;
+	var marineDigestIt = 0;
+	var uniqueMethod = 0;
+	var hotWeather = 0;
+	var dryCamper = 0;
+	var fullHookup = 0;
+	var fullTimer = 0;
+	var biannualTank = 0;
 
-// Keep Track Of Product links
-var rvDigestItLiquid = 0;
-var rvDigestItPowder = 0;
-var rvDigestItDropIn = 0;
-var rvToiletCleaner = 0;
-var tankCleaner = 0;
-var sensorCleaner = 0;
-var tankOdor = 0;
-var marineDigestIt = 0;
-var uniqueMethod = 0;
-var hotWeather = 0;
-var dryCamper = 0;
-var fullHookup = 0;
-var fullTimer = 0;
-var biannualTank = 0;
-
-for (var i = 0; i < links.length; i++) {
-	// Sort Through Links and Find Products
-	var text;
-	// Once Links Match Product Push InnerHTML to Array
-	switch (links[i].href) {
-		case "https://uniquecampingmarine.com/collections/rv-products/products/rv-digest-it-holding-tank-treatment":
-		case "https://uniquecampingmarine.com/collections/all/products/rv-digest-it-holding-tank-treatment":
-		case "https://uniquecampingmarine.com/products/rv-digest-it-holding-tank-treatment":
-			if (rvDigestItLiquid <= 0) {
-				text = `<a href="${links[i].href}" alt="RV Digest-it Holding Tank Treatment Liquid. Unique Camping + Marine">RV Digest-It | Liquid</a>`;
-				linkArr.push(text);
-				rvDigestItLiquid++;
-			} else if (rvDigestItLiquid > 0) {
+	for (var i = 0; i < links.length; i++) {
+		// Sort Through Links and Find Products
+		var text;
+		// Once Links Match Product Push InnerHTML to Array
+		switch (links[i].href) {
+			case "https://uniquecampingmarine.com/collections/rv-products/products/rv-digest-it-holding-tank-treatment":
+			case "https://uniquecampingmarine.com/collections/all/products/rv-digest-it-holding-tank-treatment":
+			case "https://uniquecampingmarine.com/products/rv-digest-it-holding-tank-treatment":
+				if (rvDigestItLiquid <= 0) {
+					text = `<a href="${links[i].href}" title="View Product" target="_blank" rel="noopener noreferrer">RV Digest-It | Liquid</a>`;
+					linkArr.push(text);
+					rvDigestItLiquid++;
+				} else if (rvDigestItLiquid > 0) {
+					break;
+				}
 				break;
-			}
-			break;
-		case "https://uniquecampingmarine.com/collections/rv-products/products/rv-digest-it-drop-in-pods":
-		case "https://uniquecampingmarine.com/collections/all/products/rv-digest-it-drop-in-pods":
-		case "https://uniquecampingmarine.com/products/rv-digest-it-drop-in-pods":
-			if (rvDigestItDropIn <= 0) {
-				text = `<a href="${links[i].href}" alt="RV Digest-it Holding Tank Treatment Drop-In. Unique Camping + Marine">RV Digest-It | Drop-In</a>`;
-				linkArr.push(text);
-				rvDigestItDropIn++;
-			} else if (rvDigestItDropIn > 0) {
+			case "https://uniquecampingmarine.com/collections/rv-products/products/rv-digest-it-drop-in-pods":
+			case "https://uniquecampingmarine.com/collections/all/products/rv-digest-it-drop-in-pods":
+			case "https://uniquecampingmarine.com/products/rv-digest-it-drop-in-pods":
+				if (rvDigestItDropIn <= 0) {
+					text = `<a href="${links[i].href}" title="View Product" target="_blank" rel="noopener noreferrer">RV Digest-It | Drop-In</a>`;
+					linkArr.push(text);
+					rvDigestItDropIn++;
+				} else if (rvDigestItDropIn > 0) {
+					break;
+				}
 				break;
-			}
-			break;
-		case "https://uniquecampingmarine.com/collections/rv-products/products/rv-digest-it-powder-treatment":
-		case "https://uniquecampingmarine.com/collections/all/products/rv-digest-it-powder-treatment":
-		case "https://uniquecampingmarine.com/products/rv-digest-it-powder-treatment":
-			if (rvDigestItPowder <= 0) {
-				text = `<a href="${links[i].href}" alt="RV Digest-it Holding Tank Treatment Powder. Unique Camping + Marine">RV Digest-It | Powder</a>`;
-				linkArr.push(text);
-				rvDigestItPowder++;
-			} else if (rvDigestItPowder > 0) {
+			case "https://uniquecampingmarine.com/collections/rv-products/products/rv-digest-it-powder-treatment":
+			case "https://uniquecampingmarine.com/collections/all/products/rv-digest-it-powder-treatment":
+			case "https://uniquecampingmarine.com/products/rv-digest-it-powder-treatment":
+				if (rvDigestItPowder <= 0) {
+					text = `<a href="${links[i].href}" title="View Product" target="_blank" rel="noopener noreferrer">RV Digest-It | Powder</a>`;
+					linkArr.push(text);
+					rvDigestItPowder++;
+				} else if (rvDigestItPowder > 0) {
+					break;
+				}
 				break;
-			}
-			break;
-		case "https://uniquecampingmarine.com/collections/rv-products/products/rv-toilet-bowl-cleaner-holding-tank-enhancer":
-		case "https://uniquecampingmarine.com/collections/all/products/rv-toilet-bowl-cleaner-holding-tank-enhancer":
-		case "https://uniquecampingmarine.com/products/rv-toilet-bowl-cleaner-holding-tank-enhancer":
-			if (rvToiletCleaner <= 0) {
-				text = `<a href="${links[i].href}" alt="RV Toilet Cleaner + Holding Tank Enhancer. Unique Camping + Marine">RV Toilet Cleaner + Holding Tank Enhancer</a>`;
-				linkArr.push(text);
-				rvToiletCleaner++;
-			} else if (rvToiletCleaner > 0) {
+			case "https://uniquecampingmarine.com/collections/rv-products/products/rv-toilet-bowl-cleaner-holding-tank-enhancer":
+			case "https://uniquecampingmarine.com/collections/all/products/rv-toilet-bowl-cleaner-holding-tank-enhancer":
+			case "https://uniquecampingmarine.com/products/rv-toilet-bowl-cleaner-holding-tank-enhancer":
+				if (rvToiletCleaner <= 0) {
+					text = `<a href="${links[i].href}" title="View Product" target="_blank" rel="noopener noreferrer">RV Toilet Cleaner + Holding Tank Enhancer</a>`;
+					linkArr.push(text);
+					rvToiletCleaner++;
+				} else if (rvToiletCleaner > 0) {
+					break;
+				}
 				break;
-			}
-			break;
-		case "https://uniquecampingmarine.com/collections/rv-products/products/rv-tank-cleaner":
-		case "https://uniquecampingmarine.com/collections/all/products/rv-tank-cleaner":
-		case "https://uniquecampingmarine.com/products/rv-tank-cleaner":
-			if (tankCleaner <= 0) {
-				text = `<a href="${links[i].href}" alt="Holding Tank Cleaner. Unique Camping + Marine">Holding Tank Cleaner</a>`;
-				linkArr.push(text);
-				tankCleaner++;
-			} else if (tankCleaner > 0) {
+			case "https://uniquecampingmarine.com/collections/rv-products/products/rv-tank-cleaner":
+			case "https://uniquecampingmarine.com/collections/all/products/rv-tank-cleaner":
+			case "https://uniquecampingmarine.com/products/rv-tank-cleaner":
+				if (tankCleaner <= 0) {
+					text = `<a href="${links[i].href}" title="View Product" target="_blank" rel="noopener noreferrer">Holding Tank Cleaner</a>`;
+					linkArr.push(text);
+					tankCleaner++;
+				} else if (tankCleaner > 0) {
+					break;
+				}
 				break;
-			}
-			break;
-		case "https://uniquecampingmarine.com/collections/rv-products/products/sensor-cleaner":
-		case "https://uniquecampingmarine.com/collections/all/products/sensor-cleaner":
-		case "https://uniquecampingmarine.com/products/sensor-cleaner":
-			if (sensorCleaner <= 0) {
-				text = `<a href="${links[i].href}" alt="Sensor Cleaner for Black Tanks. Unique Camping + Marine">Sensor Cleaner</a>`;
-				linkArr.push(text);
-				sensorCleaner++;
-			} else if (sensorCleaner > 0) {
+			case "https://uniquecampingmarine.com/collections/rv-products/products/sensor-cleaner":
+			case "https://uniquecampingmarine.com/collections/all/products/sensor-cleaner":
+			case "https://uniquecampingmarine.com/products/sensor-cleaner":
+				if (sensorCleaner <= 0) {
+					text = `<a href="${links[i].href}" title="View Product" target="_blank" rel="noopener noreferrer">Sensor Cleaner</a>`;
+					linkArr.push(text);
+					sensorCleaner++;
+				} else if (sensorCleaner > 0) {
+					break;
+				}
 				break;
-			}
-			break;
-		case "https://uniquecampingmarine.com/collections/rv-products/products/tank-odor-eliminator-drop-in-pods":
-		case "https://uniquecampingmarine.com/collections/all/products/tank-odor-eliminator-drop-in-pods":
-		case "https://uniquecampingmarine.com/products/tank-odor-eliminator-drop-in-pods":
-			if (tankOdor <= 0) {
-				text = `<a href="${links[i].href}" alt="Tank Odor Eliminator. Unique Camping + Marine">Tank Odor Eliminator</a>`;
-				linkArr.push(text);
-				tankOdor++;
-			} else if (tankOdor) {
+			case "https://uniquecampingmarine.com/collections/rv-products/products/tank-odor-eliminator-drop-in-pods":
+			case "https://uniquecampingmarine.com/collections/all/products/tank-odor-eliminator-drop-in-pods":
+			case "https://uniquecampingmarine.com/products/tank-odor-eliminator-drop-in-pods":
+				if (tankOdor <= 0) {
+					text = `<a href="${links[i].href}" title="View Product" target="_blank" rel="noopener noreferrer">Tank Odor Eliminator</a>`;
+					linkArr.push(text);
+					tankOdor++;
+				} else if (tankOdor) {
+					break;
+				}
 				break;
-			}
-			break;
-		case "https://uniquecampingmarine.com/collections/marine-products/products/marine-digest-it":
-		case "https://uniquecampingmarine.com/collections/all/products/marine-digest-it":
-		case "https://uniquecampingmarine.com/products/marine-digest-it":
-			if (marineDigestIt <= 0) {
-				text = `<a href="${links[i].href}" alt="Marine Digest-It. Unique Camping + Marine">Marine Digest-It</a>`;
-				linkArr.push(text);
-				marineDigestIt++;
-			} else if (marineDigestIt > 0) {
+			case "https://uniquecampingmarine.com/collections/marine-products/products/marine-digest-it":
+			case "https://uniquecampingmarine.com/collections/all/products/marine-digest-it":
+			case "https://uniquecampingmarine.com/products/marine-digest-it":
+				if (marineDigestIt <= 0) {
+					text = `<a href="${links[i].href}" title="View Product" target="_blank" rel="noopener noreferrer">Marine Digest-It</a>`;
+					linkArr.push(text);
+					marineDigestIt++;
+				} else if (marineDigestIt > 0) {
+					break;
+				}
 				break;
-			}
-			break;
-		case "https://uniquecampingmarine.com/collections/rv-products/products/the-unique-method-product-pack":
-		case "https://uniquecampingmarine.com/collections/all/products/the-unique-method-product-pack":
-		case "https://uniquecampingmarine.com/products/the-unique-method-product-pack":
-			if (uniqueMethod <= 0) {
-				text = `<a href="${links[i].href}" alt="The Unique Method Product Pack. Unique Camping + Marine">Unique Method Product Pack</a>`;
-				linkArr.push(text);
-				uniqueMethod++;
-			} else if (uniqueMethod > 0) {
+			case "https://uniquecampingmarine.com/collections/rv-products/products/the-unique-method-product-pack":
+			case "https://uniquecampingmarine.com/collections/all/products/the-unique-method-product-pack":
+			case "https://uniquecampingmarine.com/products/the-unique-method-product-pack":
+				if (uniqueMethod <= 0) {
+					text = `<a href="${links[i].href}" title="View Product" target="_blank" rel="noopener noreferrer">Unique Method Product Pack</a>`;
+					linkArr.push(text);
+					uniqueMethod++;
+				} else if (uniqueMethod > 0) {
+					break;
+				}
 				break;
-			}
-			break;
-		case "https://uniquecampingmarine.com/collections/rv-products/products/hot-weather-camper-pack":
-		case "https://uniquecampingmarine.com/collections/all/products/hot-weather-camper-pack":
-		case "https://uniquecampingmarine.com/products/hot-weather-camper-pack":
-			if (hotWeather <= 0) {
-				text = `<a href="${links[i].href}" alt="Hot Weather Camper Pack. Unique Camping + Marine">Hot Weather Camper Pack</a>`;
-				linkArr.push(text);
-				hotWeather++;
-			} else if (hotWeather > 0) {
+			case "https://uniquecampingmarine.com/collections/rv-products/products/hot-weather-camper-pack":
+			case "https://uniquecampingmarine.com/collections/all/products/hot-weather-camper-pack":
+			case "https://uniquecampingmarine.com/products/hot-weather-camper-pack":
+				if (hotWeather <= 0) {
+					text = `<a href="${links[i].href}" title="View Product" target="_blank" rel="noopener noreferrer">Hot Weather Camper Pack</a>`;
+					linkArr.push(text);
+					hotWeather++;
+				} else if (hotWeather > 0) {
+					break;
+				}
 				break;
-			}
-			break;
-		case "https://uniquecampingmarine.com/collections/rv-products/products/dry-camper-pack":
-		case "https://uniquecampingmarine.com/collections/all/products/dry-camper-pack":
-		case "https://uniquecampingmarine.com/products/dry-camper-pack":
-			if (dryCamper <= 0) {
-				text = `<a href="${links[i].href}" alt="Dry Camper Pack. Unique Camping + Marine">Dry Camper Pack</a>`;
-				linkArr.push(text);
-				dryCamper++;
-			} else if (dryCamper > 0) {
+			case "https://uniquecampingmarine.com/collections/rv-products/products/dry-camper-pack":
+			case "https://uniquecampingmarine.com/collections/all/products/dry-camper-pack":
+			case "https://uniquecampingmarine.com/products/dry-camper-pack":
+				if (dryCamper <= 0) {
+					text = `<a href="${links[i].href}" title="View Product" target="_blank" rel="noopener noreferrer">Dry Camper Pack</a>`;
+					linkArr.push(text);
+					dryCamper++;
+				} else if (dryCamper > 0) {
+					break;
+				}
 				break;
-			}
-			break;
-		case "https://uniquecampingmarine.com/collections/rv-products/products/full-hookup-camping-pack":
-		case "https://uniquecampingmarine.com/collections/all/products/full-hookup-camping-pack":
-		case "https://uniquecampingmarine.com/products/full-hookup-camping-pack":
-			if (fullHookup <= 0) {
-				text = `<a href="${links[i].href}" alt="Full Hookup Camper Pack. Unique Camping + Marine">Full Hookup Camper Pack</a>`;
-				linkArr.push(text);
-				fullHookup++;
-			} else if (fullHookup > 0) {
+			case "https://uniquecampingmarine.com/collections/rv-products/products/full-hookup-camping-pack":
+			case "https://uniquecampingmarine.com/collections/all/products/full-hookup-camping-pack":
+			case "https://uniquecampingmarine.com/products/full-hookup-camping-pack":
+				if (fullHookup <= 0) {
+					text = `<a href="${links[i].href}" title="View Product" target="_blank" rel="noopener noreferrer">Full Hookup Camper Pack</a>`;
+					linkArr.push(text);
+					fullHookup++;
+				} else if (fullHookup > 0) {
+					break;
+				}
 				break;
-			}
-			break;
-		case "https://uniquecampingmarine.com/collections/rv-products/products/full-timer-pack":
-		case "https://uniquecampingmarine.com/collections/all/products/full-timer-pack":
-		case "https://uniquecampingmarine.com/products/full-timer-pack":
-			if (fullTimer <= 0) {
-				text = `<a href="${links[i].href}" alt="Full Timer Camper Pack. Unique Camping + Marine">Full Timer Camper Pack</a>`;
-				linkArr.push(text);
-				fullTimer++;
-			} else if (fullTimer > 0) {
+			case "https://uniquecampingmarine.com/collections/rv-products/products/full-timer-pack":
+			case "https://uniquecampingmarine.com/collections/all/products/full-timer-pack":
+			case "https://uniquecampingmarine.com/products/full-timer-pack":
+				if (fullTimer <= 0) {
+					text = `<a href="${links[i].href}" title="View Product" target="_blank" rel="noopener noreferrer">Full Timer Camper Pack</a>`;
+					linkArr.push(text);
+					fullTimer++;
+				} else if (fullTimer > 0) {
+					break;
+				}
 				break;
-			}
-			break;
-		case "https://uniquecampingmarine.com/collections/rv-products/products/tank-maintenance-pack":
-		case "https://uniquecampingmarine.com/collections/all/products/tank-maintenance-pack":
-		case "https://uniquecampingmarine.com/products/tank-maintenance-pack":
-			if (biannualTank <= 0) {
-				text = `<a href="${links[i].href}" alt="Biannual Tank Maintenance Pack. Unique Camping + Marine">Biannual Tank Maintenance Pack</a>`;
-				linkArr.push(text);
-				biannualTank++;
-			} else if (biannualTank > 0) {
+			case "https://uniquecampingmarine.com/collections/rv-products/products/tank-maintenance-pack":
+			case "https://uniquecampingmarine.com/collections/all/products/tank-maintenance-pack":
+			case "https://uniquecampingmarine.com/products/tank-maintenance-pack":
+				if (biannualTank <= 0) {
+					text = `<a href="${links[i].href}" title="View Product" target="_blank" rel="noopener noreferrer">Biannual Tank Maintenance Pack</a>`;
+					linkArr.push(text);
+					biannualTank++;
+				} else if (biannualTank > 0) {
+					break;
+				}
 				break;
-			}
-			break;
-		default:
+			default:
+		}
 	}
-}
 
-// Clean Up Duplicates
-var uniqueLinks = linkArr.filter(function (item, index) {
-	return linkArr.indexOf(item) >= index;
-});
-
-// Sorts Links Alphabetically
-uniqueLinks.sort();
-
-if (productDiv) {
-	// Append Links
-	uniqueLinks.forEach(function (product) {
-		var bullet = document.createElement("li");
-		bullet.innerHTML = product;
-		productList.appendChild(bullet);
+	// Clean Up Duplicates
+	var uniqueLinks = linkArr.filter(function (item, index) {
+		return linkArr.indexOf(item) >= index;
 	});
-}
 
-// If Links, Show Product Section
-if (productDiv && linkArr.length) {
-	productDiv.style.display = "block";
+	// Sorts Links Alphabetically | *URLs added dynamically from the for loop above may throw off order
+	uniqueLinks.sort();
+
+	// Append Links
+	if (productDiv) {
+		uniqueLinks.forEach(function (product) {
+			var bullet = document.createElement("li");
+			bullet.innerHTML = product;
+			productList.appendChild(bullet);
+		});
+	}
+
+	// If Links, Show Product Section
+	if (productDiv && linkArr.length) {
+		productDiv.style.display = "block";
+	}
 }
